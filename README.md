74a089296c02217d27307010a569fecd8d0a5cbc
lutaylor <luke@discoverygarden.ca>
2018-06-08 13:10:28 -0300

    adding in updates for Tomcat logging.

--- README.md
+++ README.md
@@ -3,12 +3,24 @@ islandora_log_config
 
 Rotates the logs created by Islandora, fedora, activemq,  gsearch and tomcat.
 
-Will need to be modified to include mcroservices and batch ingest.
+Will need to be modified to include microservices and batch ingest.
 The first user who needs this should submit the patch.
 
-file locations default
+General Usage:
+
+git clone --recursive git@github.com:discoverygarden/islandora_log_config.git
+cd islandora_log_config
+sudo ./install.sh
+
+File locations default
 
 /etc/logrotate.d/islandora_logrotate
+
+/etc/logrotate.d/islandora_log
+
+/etc/logrotate.d/blazegraph_log
+
+/etc/logrotate.d/drupal_syslog
 
 /usr/local/fedora/tomcat/webapps/fedoragsearch/WEB-INF/classes/log4j.xml
 
@@ -17,3 +29,6 @@ file locations default
 /usr/local/fedora/tomcat/webapps/adore-djatoka/WEB-INF/classes/log4j.properties
 
 /usr/local/fedora/server/config/logback.xml
+
+/usr/local/fedora/tomcat/conf/server.xml
+
--- install.sh
+++ install.sh
@@ -24,14 +24,63 @@ else
 fi
 
 # Copy files for backend server
+# This is assuming stock legacy install locations.
 if [ -z $CATALINA_HOME ];then
   echo '$CATALINA_HOME is undefined'
   echo "Skipping log configs for backend servers"
 else
-  echo "Copying log configs for a backend server"
-  cp islandora_logrotate /etc/logrotate.d/
-  chmod 644 /etc/logrotate.d/islandora_logrotate
-  cp log4j.xml          $CATALINA_HOME/webapps/fedoragsearch/WEB-INF/classes/log4j.xml
-  cp logging.properties $CATALINA_HOME/conf/logging.properties
-  cp log4j.properties   $CATALINA_HOME/webapps/adore-djatoka/WEB-INF/classes/log4j.properties
+  if [ -d /usr/local/fedora/tomcat/conf ]; then
+    echo "Copying log configs for a backend server with Fedora"
+    cp islandora_logrotate /etc/logrotate.d/
+    chmod 644 /etc/logrotate.d/islandora_logrotate
+    cp log4j.xml          /usr/local/fedora/webapps/fedoragsearch/WEB-INF/classes/log4j.xml
+    cp logging.properties /usr/local/fedora/tomcat/conf/logging.properties
+    cp fedora_tomcat_conf_sample/server.xml /usr/local/fedora/tomcat/conf/server.xml
+    cp log4j.properties   /usr/local/fedora/tomcat/webapps/adore-djatoka/WEB-INF/classes/log4j.properties
+    if [ -d /usr/share/tomcat-blzg/conf ]; then
+      echo "Blazegraph has been installed on same server as Fedora"
+      echo "Please enter desired port numbers that will be used in server.xml"
+      read -p "Enter HTTP connector port: [8081]" httpc
+      read -p "Enter HTTPS connector port: [8444]" httpsc
+      read -p "Enter AJP connector port: [8010]" ajpc
+      read -p "Enter shutdown port: [8006]" shutdownc
+
+      echo "Copying log configs for a backend server with Blazegraph"
+      cp blazegraph_log /etc/logrotate.d/
+      chmod 644 /etc/logrotate.d/blazegraph_log
+      cp logging.properties /usr/share/tomcat-blzg/conf/logging.properties
+      cp fedora_tomcat_conf_sample/non_fedora_server.xml /usr/share/tomcat-blzg/conf/server.xml
+      sed -i "s|8005|$shutdownc|g" /usr/share/tomcat-blzg/conf/server.xml
+      sed -i "s|8009|$ajpc|g" /usr/share/tomcat-blzg/conf/server.xml
+      sed -i "s|8080|$httpc|g" /usr/share/tomcat-blzg/conf/server.xml
+      sed -i "s|8443|$httpsc|g" /usr/share/tomcat-blzg/conf/server.xml
+      echo "Blazegraph server.xml has been updated with logging changes."
+      echo "Require manual service restart of Blazegraph..."
+    fi
+    echo "Fedora server.xml has been updated with logging changes."
+    echo "Require manual service restart of Fedora..."
+  else
+    if [ -d /usr/share/tomcat-blzg/conf ]; then
+      echo "Copying log configs for a backend server with Blazegraph"
+      cp blazegraph_log /etc/logrotate.d/
+      chmod 644 /etc/logrotate.d/blazegraph_log
+      cp logging.properties /usr/share/tomcat-blzg/conf/logging.properties
+      cp fedora_tomcat_conf_sample/non_fedora_server.xml /usr/share/tomcat-blzg/conf/server.xml
+      echo "Blazegraph server.xml has been updated with logging changes."
+      echo "Require manual service restart of Blazegraph..."
+    elif [ -d /usr/share/tomcat/conf]; then
+      echo "Copying log configs for a backend server with generic Tomcat"
+      cp generic_tomcat_log /etc/logrotate.d/
+      chmod 644 /etc/logrotate.d/generic_tomcat_log
+      cp logging.properties /usr/share/tomcat/conf/logging.properties
+      cp fedora_tomcat_conf_sample/non_fedora_server.xml /usr/share/tomcat/conf/server.xml
+      echo "Tomcat server.xml has been updated with logging changes."
+      echo "Require manual service restart of Tomcat..."
+    else 
+      echo "Tomcat in non-standard location or was installed from binaries"
+      echo "Not placing any backend logging configuration."
+      echo "Manual review required..."
+      exit 2
+    done
+  fi
 fi
